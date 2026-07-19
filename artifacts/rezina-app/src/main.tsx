9c97e8bb46a93c216d826b36ccccc2d67f1c50bc
Dordea Pavel <pavlik@duck.com>
2026-07-19 23:46:36 +0300

    Fix GitHub Pages routing for admin

--- artifacts/rezina-app/src/main.tsx
+++ artifacts/rezina-app/src/main.tsx
@@ -2,6 +2,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+
+if (redirect && redirect !== location.pathname) {
+  history.replaceState(null, '', redirect);
+}
+
 createRoot(document.getElementById('root')!).render(
   <App />
 )
